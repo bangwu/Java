27f36dd5bd45bef63ad9c9bc1f4baaa6d35c68ba
wubang <wolfwubang@gmail.com>
2014-10-27 17:00:50 +0800

    chapter01 test

---
+++ CleanCode/src/hue/edu/imis/chapter01/HelloWorld.java
@@ -0,0 +1,10 @@
+package hue.edu.imis.chapter01;
+
+/**
+ * Created by bang on 2014/10/27.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
